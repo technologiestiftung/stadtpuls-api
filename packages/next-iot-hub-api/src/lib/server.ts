789559a0bdb485f78445923e7b580f805d44ebec
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-12 11:05:19 +0200

    test(refactor server): Refactor server for easier testing

--- packages/next-iot-hub-api/src/lib/server.ts
+++ packages/next-iot-hub-api/src/lib/server.ts
@@ -1,24 +1,35 @@
 import "make-promises-safe";
-import fastify from "fastify";
+import fastify, { FastifyInstance } from "fastify";
 import fastifyBlipp from "fastify-blipp";
 import fastifyJwt from "fastify-jwt";
 import fastifyHelmet from "fastify-helmet";
 import fastifyCors from "fastify-cors";
 import fastifySensible from "fastify-sensible";
 import fastifySupabase from "@technologiestiftung/fastify-supabase";
-import { jwtSecret, supabaseUrl, supabaseServiceRoleKey } from "./env";
 
 import routes from "./routes";
-const server = fastify({ logger: true });
 
-server.register(fastifyHelmet);
-server.register(fastifyCors);
-server.register(fastifySensible);
-server.register(fastifyJwt, {
-  secret: jwtSecret,
-});
-server.register(fastifyBlipp);
-server.register(fastifySupabase, { supabaseUrl, supabaseServiceRoleKey });
-server.register(routes);
+export const buildServer: (options: {
+  jwtSecret: string;
+  supabaseUrl: string;
+  supabaseServiceRoleKey: string;
+}) => FastifyInstance = ({
+  jwtSecret,
+  supabaseUrl,
+  supabaseServiceRoleKey,
+}) => {
+  const server = fastify({ logger: true });
 
-export default server;
+  server.register(fastifyHelmet);
+  server.register(fastifyCors);
+  server.register(fastifySensible);
+  server.register(fastifyJwt, {
+    secret: jwtSecret,
+  });
+  server.register(fastifyBlipp);
+  server.register(fastifySupabase, { supabaseUrl, supabaseServiceRoleKey });
+  server.register(routes);
+  return server;
+};
+
+export default buildServer;
