45b6d9f28ad91bc8024d96755aba5fcf48d68d19
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-12 22:16:55 +0200

    feature(seed): Add generative seed script

---
+++ packages/next-iot-hub-db/supabase/seed.sql
@@ -0,0 +1,82 @@
+insert into
+  users (id, name)
+values
+(
+    uuid_generate_v4 (),
+    substring(
+      (md5(random() :: text))
+      from
+        0 for 19
+    )
+  );
+
+insert into
+  categories ("name", description)
+values
+  ('CO2', 'foo');
+
+insert into
+  projects ("userId", name, "categoryId")
+values
+(
+    (
+      select
+        id
+      from
+        users
+      order by
+        random()
+      limit
+        1
+    ), md5(random() :: text), (
+      select
+        id
+      from
+        categories
+      order by
+        random()
+      limit
+        1
+    )
+  );
+
+insert into
+  devices ("userId", externalid, "projectId")
+values
+(
+    (
+      select
+        id
+      from
+        users
+      order by
+        random()
+      limit
+        1
+    ), 'foo', (
+      select
+        id
+      from
+        projects
+      order by
+        random()
+      limit
+        1
+    )
+  );
+
+insert into
+  records ("deviceId", "recordedAt")
+values
+(
+    (
+      select
+        id
+      from
+        devices
+      order by
+        random()
+      limit
+        1
+    ), now()
+  );
