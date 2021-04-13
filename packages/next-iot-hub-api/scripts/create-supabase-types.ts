3ce4d134edffe2445a06de9d3fb2fd97fc5a62c6
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-13 21:22:42 +0200

    feature(types): Generate types from supabase

---
+++ packages/next-iot-hub-api/scripts/create-supabase-types.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import path from "path";
+const swaggerToTS = require("openapi-typescript").default;
+
+import https from "https";
+const anonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.SUPABASE_URL;
+let spec = "";
+const url = new URL(`${supabaseUrl}/rest/v1/?apikey=${anonKey}`);
+// const options = {
+//   hostname: url.host,
+//   port: 443,
+//   path: url.pathname,
+//   method: "GET",
+// };
+const req = https.request(url, (res) => {
+  console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+    // process.stdout.write(d);
+    spec += d;
+  });
+  res.on("end", () => {
+    // console.log(spec);
+
+    const input = JSON.parse(spec); // Input can be any JS object (OpenAPI format)
+    const output = swaggerToTS(input); // Outputs TypeScript defs as a string (to be parsed, or written to a file)
+    console.log(output);
+    fs.writeFile(
+      path.resolve(process.cwd(), "./src/common/supabase.ts"),
+      output,
+      "utf8",
+      (err) => {
+        if (err) throw err;
+      }
+    );
+  });
+});
+
+req.on("error", (error) => {
+  console.error(error);
+});
+
+req.end();
