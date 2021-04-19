cf2264e68903b65dd188af82e783a70d80ce2220
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-19 14:08:15 +0200

    arrrrgggghhhhhhh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-npm install pnpm@6.0.2
 ./node_modules/.bin/pnpm multi install
 ./node_modules/.bin/pnpm run -r build --filter "*fastify-supabase"
 ./node_modules/.bin/pnpm run -r build --filter "*next-iot-hub-api"
