4b44537a0bb5667bd7cb5f4289ba520337d9e293
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-04-20 14:24:33 +0200

    feature(ttn): This adds the first ttn integration
    woooooohoooooo!!! \o/

--- src/integrations/ttn.ts
+++ src/integrations/ttn.ts
@@ -1,18 +1,90 @@
-import { FastifyPluginAsync, FastifyReply, FastifyRequest } from "fastify";
+import { FastifyPluginAsync } from "fastify";
 import fp from "fastify-plugin";
 import { compare } from "bcrypt";
 import { definitions } from "../common/supabase";
 import { AuthToken } from "../common/jwt";
+import S from "fluent-json-schema";
+
 declare module "fastify" {
   interface FastifyInstance {
     verifyJWT: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
   }
 }
 
+interface TTNPostBody {
+  [key: string]: unknown;
+  received_at: string;
+  uplink_message: {
+    decoded_payload: {
+      bytes: number[];
+      measurements: number[];
+    };
+    locations?: {
+      user?: {
+        latitude: number;
+        longitude: number;
+        altitude?: number;
+        source: "SOURCE_REGISTRY" | string;
+      };
+    };
+  };
+  end_device_ids: {
+    dev_eui: "string";
+    device_id: "string";
+    application_ids: {
+      application_id: "string";
+    };
+  };
+  simulated: boolean;
+}
+const postTTNBodySchema = S.object()
+  .id("/integrations/ttn/v3")
+  .title("Validation for data coming from TTN")
+  .additionalProperties(true)
+
+  .prop(
+    "end_device_ids",
+    S.object()
+      .prop("device_id", S.string())
+      .required()
+      .prop("dev_eui", S.string())
+      .required()
+  )
+  .required()
+  .additionalProperties(true)
+  .prop("received_at", S.string().format(S.FORMATS.DATE_TIME))
+  .required()
+  .prop(
+    "uplink_message",
+    S.object()
+      .prop(
+        "decoded_payload",
+        S.object().prop("measurements", S.array().items(S.number()).required())
+      )
+      .required()
+      .additionalProperties(true)
+
+      .prop(
+        "locations",
+        S.object()
+          .prop(
+            "user",
+            S.object()
+              .prop("latitude", S.number())
+              .prop("longitude", S.number())
+          )
+          .additionalProperties(true)
+      )
+      .additionalProperties(true)
+  )
+  .required()
+  .additionalProperties(true);
+// console.log(JSON.stringify(postTTNBodySchema.valueOf(), null, 2));
 const ttn: FastifyPluginAsync = async (fastify) => {
-  fastify.route({
+  fastify.route<{ Body: TTNPostBody }>({
     url: "/api/v2/integrations/ttn/v3",
     method: "POST",
+    schema: { body: postTTNBodySchema },
     preHandler: fastify.auth([fastify.verifyJWT]),
     handler: async (request, reply) => {
       const decoded = (await request.jwtVerify()) as AuthToken;
@@ -25,20 +97,65 @@ const ttn: FastifyPluginAsync = async (fastify) => {
         .select("*")
         .eq("userId", decoded.sub)
         .eq("projectId", decoded.projectId);
+      if (!authtokens || authtokens.length === 0) {
+        fastify.log.warn("no token found");
+        throw fastify.httpErrors.unauthorized();
+      }
 
       if (error) {
+        fastify.log.error("postgres error");
         throw fastify.httpErrors.internalServerError();
-      }
-      if (!authtokens) {
-        throw fastify.httpErrors.unauthorized();
       }
 
       const compared = await compare(token, authtokens[0].id);
       if (!compared) {
+        fastify.log.warn("using old token");
         throw fastify.httpErrors.unauthorized();
       }
-      console.log(request.body);
-      reply.status(201).send({ compared });
+      const { end_device_ids, received_at, uplink_message } = request.body;
+      const { dev_eui } = end_device_ids;
+      const { decoded_payload, locations } = uplink_message;
+      const { measurements } = decoded_payload;
+      const { data: devices, error: deviceError } = await fastify.supabase
+        .from("devices")
+        .select("*")
+        .eq("externalId", dev_eui)
+        .eq("projectId", decoded.projectId)
+        .eq("userId", decoded.sub);
+      if (!devices || devices.length === 0) {
+        throw fastify.httpErrors.notFound("device not found");
+      }
+      if (deviceError) {
+        throw fastify.httpErrors.internalServerError(
+          "device not found postgres error"
+        );
+      }
+      console.log(
+        new Date(received_at).toISOString().replace("T", " ").replace("Z", "")
+      );
+      const { data: record, error: recordError } = await fastify.supabase
+        .from<definitions["records"]>("records")
+        .insert([
+          {
+            measurements: `{${measurements.join(",")}}`,
+            recordedAt: received_at,
+            deviceId: devices[0].id,
+            latitude: locations?.user?.latitude,
+            longitude: locations?.user?.longitude,
+            altitude: locations?.user?.altitude,
+          },
+        ]);
+
+      if (!record) {
+        throw fastify.httpErrors.internalServerError("could not create record");
+      }
+      if (recordError) {
+        throw fastify.httpErrors.internalServerError(
+          "could not create record postgres error"
+        );
+      }
+
+      reply.status(201).send({ record });
     },
   });
 };
