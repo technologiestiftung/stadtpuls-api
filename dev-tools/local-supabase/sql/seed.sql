ea49b292b16947cdce8ac9ce15596f81c077c39a
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-05-17 16:55:51 +0200

    fix(seed): Add random measurements

--- dev-tools/local-supabase/sql/seed.sql
+++ dev-tools/local-supabase/sql/seed.sql
@@ -1,45 +1,83 @@
-insert into userprofiles (id, name)
-values (
+insert into
+  userprofiles (id, name)
+values
+  (
     uuid_generate_v4 (),
     substring(
-      (md5(random()::text))
-      from 0 for 19
+      (md5(random() :: text))
+      from
+        0 for 19
     )
   );
-insert into projects ("userId", name, "categoryId")
-values (
+
+insert into
+  projects ("userId", name, "categoryId")
+values
+  (
     (
-      select id
-      from userprofiles
-      order by random()
-      limit 1
-    ), md5(random()::text), (
-      select id
-      from categories
-      order by random()
-      limit 1
+      select
+        id
+      from
+        userprofiles
+      order by
+        random()
+      limit
+        1
+    ), md5(random() :: text), (
+      select
+        id
+      from
+        categories
+      order by
+        random()
+      limit
+        1
     )
   );
-insert into devices ("userId", "externalId", "projectId")
-values (
+
+insert into
+  devices ("userId", "externalId", "projectId")
+values
+  (
     (
-      select id
-      from userprofiles
-      order by random()
-      limit 1
+      select
+        id
+      from
+        userprofiles
+      order by
+        random()
+      limit
+        1
     ), 'foo', (
-      select id
-      from projects
-      order by random()
-      limit 1
+      select
+        id
+      from
+        projects
+      order by
+        random()
+      limit
+        1
     )
   );
-insert into records ("deviceId", "recordedAt")
-values (
+
+insert into
+  records ("deviceId", "recordedAt", "measurements")
+values
+  (
     (
-      select id
-      from devices
-      order by random()
-      limit 1
-    ), now()
+      select
+        id
+      from
+        devices
+      order by
+        random()
+      limit
+        1
+    ), now(),
+    (
+      select
+        array_agg(round(random() * (100 - 0)) + 0)
+      from
+        generate_series (0, 10)
+    )
   );
