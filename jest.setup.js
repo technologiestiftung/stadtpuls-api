51324c833fc4accbc52c8201af4c6c4e4a7e97b2
Fabian Morón Zirfas <fabianmoronzirfas@protonmail.ch>
2021-08-11 13:10:38 +0200

    Update jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,9 @@
-process.env.SUPABASE_URL = "http://localhost:8000";
-process.env.SUPABASE_ANON_KEY =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoiYW5vbiJ9.36fUebxgx1mcBo4s19v0SzqmzunP--hm_hep0uLX0ew";
-process.env.SUPABASE_SERVICE_ROLE_KEY =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoic2VydmljZV9yb2xlIn0.necIJaiP7X2T2QjGeV-FhpkizcNTX8HjDDBAxpgQTEI";
-process.env.JWT_SECRET =
-  "super-secret-jwt-token-with-at-least-32-characters-long";
-process.env.PORT = "4000";
-process.env.ISSUER = "technologiestiftung-berlin.de";
+// process.env.SUPABASE_URL = "http://localhost:8000";
+// process.env.SUPABASE_ANON_KEY =
+//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoiYW5vbiJ9.36fUebxgx1mcBo4s19v0SzqmzunP--hm_hep0uLX0ew";
+// process.env.SUPABASE_SERVICE_ROLE_KEY =
+//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJzdXBhYmFzZSIsImlhdCI6MTYwMzk2ODgzNCwiZXhwIjoyNTUwNjUzNjM0LCJyb2xlIjoic2VydmljZV9yb2xlIn0.necIJaiP7X2T2QjGeV-FhpkizcNTX8HjDDBAxpgQTEI";
+// process.env.JWT_SECRET =
+//   "super-secret-jwt-token-with-at-least-32-characters-long";
+// process.env.PORT = "4000";
+// process.env.ISSUER = "technologiestiftung-berlin.de";
