ad6ffb1b064b30a4c8bd61d0f9197435817e6c1a
Dennis Ostendorf <15640196+dnsos@users.noreply.github.com>
2021-08-19 10:52:31 +0200

    chore(STADTPULS-316): add category descriptions to local dev supabase (#35)
    Copied from production DB

--- dev-tools/supabase/dockerfiles/postgres/docker-entrypoint-initdb.d/60-categories.sql
+++ dev-tools/supabase/dockerfiles/postgres/docker-entrypoint-initdb.d/60-categories.sql
@@ -1,9 +1,9 @@
 INSERT INTO
   "public"."categories" ("name", "description")
 VALUES
-  ('CO2', 'foo'),
-  ('Temperatur', 'foo'),
-  ('Luftfeuchtigkeit', 'foo'),
-  ('Druck', 'foo'),
-  ('PAXCounter', 'foo'),
-  ('Lautstärke', 'foo');
+  ('CO2', 'Ein CO2-Sensor kann die Kohlendioxid-Konzentration in Räumen überwachen. Für eine gute Raumluftqualität sollte der Grenzwert von 1000 ppm CO2 (Erdatmosphäre ca. 400 ppm) nicht überschritten werden.'),
+  ('Temperatur', 'Handelsübliche Temperatursensoren messen die Temperatur (meist in Grad Celsius) in einer Spanne von -55 bis 125°C. Übrigens: die mittlere globale Durchschnittstemperatur liegt bei ca. +14 °C.'),
+  ('Luftfeuchtigkeit', 'Ein Luftfeuchtesensor gibt die absolute Luftfeuchte in Gramm Wasserdampf pro m3 Luft an. Dabei gilt: Je höher die Temperatur, desto mehr Wasser kann die Luft aufnehmen (bspw. bei 30 °C ca. 30 g/m³).'),
+  ('Druck', 'Luftdrucksensoren messen den Druck, der durch die Gewichtskraft einer Luftsäule auf der Erdoberfläche entsteht. Der mittlere Luftdruck auf der Erde (Höhe = 0 m) beträgt rund 1.013 hPa bzw. 1,013 bar.'),
+  ('PAXCounter', 'PAXCounter können bspw. Handys in der Umgebung anhand ihrer WLAN- und Bluetooth-Signale zählen. Damit eignen sie sich gut, um Besucherströme und Auslastungen an einem bestimmten Ort zu bestimmen. '),
+  ('Lautstärke', 'Ein Lautstärkesensor erfasst die Umgebungsgeräusche (Lautstärke) in Form des Schalldrucks und gibt die Messwerte in Dezibel aus. Als Referenz: ein Staubsauger erreicht einen Schalldruck von ca. 70 dB.');
