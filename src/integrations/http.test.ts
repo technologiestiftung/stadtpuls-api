bffd90b8cbe38fd795738b207a0f7a7dcf944afc
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-08-26 13:34:30 +0200

    test(http): Increase coverage

--- src/integrations/http.test.ts
+++ src/integrations/http.test.ts
@@ -14,7 +14,7 @@ let userToken: string;
 let userId: string;
 const email = faker.internet.email();
 const password = faker.internet.password();
-import { deleteUser, login, logout, signup } from "../__test-utils";
+import { deleteUser, login, signup } from "../__test-utils";
 import { definitions } from "../common/supabase";
 const issuer = "tsb";
 
@@ -25,27 +25,6 @@ const httpPayload = {
   measurements: [1, 2, 3],
 };
 describe("tests for the http integration", () => {
-  beforeEach(async () => {
-    const { id, token } = await login({
-      anonKey: supabaseAnonKey,
-      email,
-      password,
-      url: new URL(`${supabaseUrl}/auth/v1/token?grant_type=password`),
-    });
-    userId = id;
-    userToken = token;
-  });
-  afterEach(async () => {
-    const success = await logout({
-      userToken,
-      anonKey: supabaseAnonKey,
-      url: new URL(`${supabaseUrl}/auth/v1/logout`),
-    });
-    if (!success) {
-      throw new Error("could not log out");
-    }
-  });
-
   beforeAll(async () => {
     const { id, token } = await signup({
       anonKey: supabaseAnonKey,
@@ -110,5 +89,205 @@ describe("tests for the http integration", () => {
     expect(response.body).toMatchInlineSnapshot(
       `"{\\"statusCode\\":400,\\"error\\":\\"Bad Request\\",\\"message\\":\\"body should be object\\"}"`
     );
+  });
+
+  test("should be rejected due to no wrong body", async () => {
+    const server = buildServer({
+      jwtSecret,
+      supabaseUrl,
+      supabaseServiceRoleKey,
+      logger: false,
+      issuer,
+    });
+    const response = await server.inject({
+      method: "POST",
+      url: "/api/v2/devices/1/records",
+      payload: {},
+    });
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toMatchInlineSnapshot(
+      `"{\\"statusCode\\":400,\\"error\\":\\"Bad Request\\",\\"message\\":\\"body should have required property 'measurements'\\"}"`
+    );
+  });
+
+  test("should be rejected due to no token", async () => {
+    const server = buildServer({
+      jwtSecret,
+      supabaseUrl,
+      supabaseServiceRoleKey,
+      logger: false,
+      issuer,
+    });
+    const response = await server.inject({
+      method: "POST",
+      url: "/api/v2/devices/1/records",
+      payload: httpPayload,
+    });
+    expect(response.statusCode).toBe(401);
+  });
+
+  test("should find no device", async () => {
+    const server = buildServer({
+      jwtSecret,
+      supabaseUrl,
+      supabaseServiceRoleKey,
+      logger: false,
+      issuer,
+    });
+    const { data: projects, error } = await supabase
+      .from<definitions["projects"]>("projects")
+      .insert([{ name: "test", userId, categoryId: 1 }]);
+    if (!projects) {
+      throw error;
+    }
+
+    const url = `/api/v2/authtokens`;
+    const responseToken = await server.inject({
+      method: "POST",
+      url,
+      headers: {
+        authorization: `Bearer ${userToken}`,
+        apikey: supabaseAnonKey,
+      },
+      payload: {
+        projectId: projects[0].id,
+        description: "testing",
+      },
+    });
+
+    const resBody = JSON.parse(responseToken.body);
+    const response = await server.inject({
+      method: "POST",
+      url: "/api/v2/devices/1/records",
+      payload: httpPayload,
+      headers: {
+        Authorization: `Bearer ${resBody.data.token}`,
+      },
+    });
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toMatchInlineSnapshot(
+      `"{\\"statusCode\\":404,\\"error\\":\\"Not Found\\",\\"message\\":\\"device not found\\"}"`
+    );
+  });
+
+  test("should find no authtoken", async () => {
+    const server = buildServer({
+      jwtSecret,
+      supabaseUrl,
+      supabaseServiceRoleKey,
+      logger: false,
+      issuer,
+    });
+    const { data: projects, error } = await supabase
+      .from<definitions["projects"]>("projects")
+      .insert([{ name: "test", userId, categoryId: 1 }]);
+    if (!projects) {
+      throw error;
+    }
+
+    const url = `/api/v2/authtokens`;
+    const responseToken = await server.inject({
+      method: "POST",
+      url,
+      headers: {
+        authorization: `Bearer ${userToken}`,
+        apikey: supabaseAnonKey,
+      },
+      payload: {
+        projectId: projects[0].id,
+        description: "testing",
+      },
+    });
+
+    const resBody = JSON.parse(responseToken.body);
+    const getResponse = await server.inject({
+      method: "GET",
+      url: `${url}?projectId=${projects[0].id}`,
+      headers: {
+        authorization: `Bearer ${userToken}`,
+        apikey: supabaseAnonKey,
+      },
+    });
+
+    const parsedGetRes = JSON.parse(getResponse.body);
+    const deleteResponse = await server.inject({
+      method: "DELETE",
+      url,
+      headers: {
+        authorization: `Bearer ${userToken}`,
+        apikey: supabaseAnonKey,
+      },
+      payload: {
+        projectId: projects[0].id,
+        tokenId: parsedGetRes.data[0].niceId,
+      },
+    });
+    const response = await server.inject({
+      method: "POST",
+      url: "/api/v2/devices/1/records",
+      payload: httpPayload,
+      headers: {
+        Authorization: `Bearer ${resBody.data.token}`,
+      },
+    });
+    expect(response.statusCode).toBe(401);
+    expect(response.body).toMatchInlineSnapshot(
+      `"{\\"statusCode\\":401,\\"error\\":\\"Unauthorized\\",\\"message\\":\\"Unauthorized\\"}"`
+    );
+  });
+
+  test("should pass", async () => {
+    const server = buildServer({
+      jwtSecret,
+      supabaseUrl,
+      supabaseServiceRoleKey,
+      logger: false,
+      issuer,
+    });
+    const { data: projects, error } = await supabase
+      .from<definitions["projects"]>("projects")
+      .insert([{ name: "test", userId, categoryId: 1 }]);
+    if (!projects) {
+      throw error;
+    }
+
+    const { data: devices, error: dError } = await supabase
+      .from<definitions["devices"]>("devices")
+      .insert([
+        {
+          name: "test",
+          userId,
+          projectId: projects[0].id,
+          // externalId: httpPayload.end_device_ids.device_id,
+        },
+      ]);
+    if (!devices) {
+      throw dError;
+    }
+
+    const url = `/api/v2/authtokens`;
+    const responseToken = await server.inject({
+      method: "POST",
+      url,
+      headers: {
+        authorization: `Bearer ${userToken}`,
+        apikey: supabaseAnonKey,
+      },
+      payload: {
+        projectId: projects[0].id,
+        description: "testing",
+      },
+    });
+
+    const resBody = JSON.parse(responseToken.body);
+    const response = await server.inject({
+      method: "POST",
+      url: `/api/v2/devices/${devices[0].id}/records`,
+      payload: httpPayload,
+      headers: {
+        Authorization: `Bearer ${resBody.data.token}`,
+      },
+    });
+    expect(response.statusCode).toBe(201);
   });
 });
