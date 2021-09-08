ff8c1db26ba8c6e626b5c61a02591ed7f69c3131
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-09-08 16:49:20 +0200

    STADTPULS-364: feat(schema v4): Update schema to latest draft version
    - This removes all traces of projects from the schema
    - renames pax counter
    - renames Druck to Luftdruck
    - adds a trigger that removes user profiles
        if they are delete form the auth schema
    - moves delete cascades to table creation
    - regenrates types from local db
    
    many test might fail!

--- dev-tools/supabase/dockerfiles/postgres/docker-entrypoint-initdb.d/50-remote-procedure-calls.sql
+++ dev-tools/supabase/dockerfiles/postgres/docker-entrypoint-initdb.d/50-remote-procedure-calls.sql
@@ -1,9 +1,22 @@
+--
+--
+--
+--
+--
+-- function to allow a user to delete his account
+-- this will nuke all his devices and records
 create or replace function public.delete_user() returns void LANGUAGE SQL SECURITY DEFINER AS $$
-delete from public.userprofiles
+delete from public.user_profiles
 where id = auth.uid();
 delete from auth.users
 where id = auth.uid();
 $$;
+--
+--
+--
+--
+--
+-- Function to allow the user to change his email
 CREATE OR REPLACE FUNCTION public.update_email(new_email text) RETURNS boolean LANGUAGE plpgsql SECURITY DEFINER AS $$ begin
 UPDATE auth.users
 set email = new_email
