9872f57f69b73a2bd656a5cc6167e8726de169db
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-09-08 11:56:02 +0200

    chore(housekeeping): Remove msw dependency

--- src/mocks/server.ts
+++
@@ -1,8 +0,0 @@
-import { setupServer, SetupServerApi } from "msw/node";
-import { setupHandlers } from "./handlers";
-
-export const setupMSWServer: (url: URL) => SetupServerApi = (url) => {
-  const handlers = setupHandlers(url);
-  const server = setupServer(...handlers);
-  return server;
-};
