3e6f3277cf7a8bb585adb8543ea6f33803049339
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-09-14 16:22:35 +0200

    test(mocks): Playing with mocks

--- src/lib/authtokens.error.test.ts
+++ src/lib/authtokens.error.test.ts
@@ -1,5 +1,8 @@
+/* eslint-disable jest/no-commented-out-tests */
+/* eslint-disable jest/no-disabled-tests */
+/* eslint-disable jest/no-hooks */
 import nock from "nock";
-
+// import * as supabase from "@supabase/supabase-js";
 import {
   authtokenEndpoint,
   buildServerOpts,
@@ -7,8 +10,17 @@ import {
   signupUser,
 } from "../__test-utils";
 import { buildServer } from "./server";
-
+// import { PostgrestError } from "@supabase/supabase-js";
+// jest.mock("@supabase/supabase-js", () => ({
+//   createClient: jest.fn(),
+// }));
 describe("error handling", () => {
+  //   afterEach(() => {
+  //     jest.resetAllMocks();
+  //   });
+  //   afterAll(() => {
+  //     jest.clearAllMocks();
+  //   });
   test("should throw an error in GET due to returned postgres error", async () => {
     const server = buildServer(buildServerOpts);
     const user = await signupUser();
@@ -35,3 +47,31 @@ describe("error handling", () => {
     await deleteUser(user.token);
   });
 });
+
+// test.only("should mock supabase and return error", async () => {
+//   const err: PostgrestError = {
+//     code: "500",
+//     message: "string",
+//     details: "string",
+//     hint: "string",
+//   };
+//   const postgrestResponse: any = {
+//     from: jest.fn().mockRejectedValue({ error: err }),
+//     select: jest.fn().mockRejectedValue({ error: err }),
+//   };
+//   //@ts-ignore
+//   supabase.createClient.mockImplementation(() => {
+//     return postgrestResponse;
+//   });
+//   const server = buildServer(buildServerOpts);
+//   const user = await signupUser();
+//   const res = await server.inject({
+//     method: "GET",
+//     url: authtokenEndpoint,
+//     headers: { authorization: `Bearer ${user.token}` },
+//   });
+//   expect(res.statusCode).toBe(500);
+//   expect(supabase.createClient).toHaveBeenCalledTimes(1);
+//   expect(postgrestResponse.select).toHaveBeenCalledTimes(1);
+//   await deleteUser(user.token);
+// });
