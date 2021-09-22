79d85554123aa96e1e4ef92b420bd575ba55ec0c
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-09-22 17:40:35 +0200

    STADTPULS-327: feat(sensor): With pagination and category filter

--- src/lib/sensors.ts
+++ src/lib/sensors.ts
@@ -2,6 +2,7 @@
 //
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
+import { PostgrestResponse } from "@supabase/supabase-js";
 import config from "config";
 import { FastifyPluginAsync } from "fastify";
 
@@ -9,12 +10,17 @@ import fp from "fastify-plugin";
 import S from "fluent-json-schema";
 import { definitions } from "../common/supabase";
 import { logLevel, supabaseMaxRows } from "./env";
-import { buildReplyPayload } from "./reply-utils";
+import {
+  buildReplyHeaders,
+  buildReplyPayload,
+  ContentRange,
+  RangeUnit,
+} from "./reply-utils";
 
-type Categories = Pick<definitions["categories"], "name">;
+// type Categories = definitions["categories"]["name"];
 type Sensor = definitions["sensors"];
 interface Querystring {
-  category?: Categories | number;
+  category_id?: number;
   limit?: number;
   offset?: number;
 }
@@ -27,13 +33,14 @@ export interface SensorsPluginOptions {
   apiVersion: string;
   endpoint: string;
 }
+type SensorNoUserId = Omit<Sensor, "user_id">;
 const apiVersion = config.get("apiVersion");
 const mountPoint = config.get<string>("mountPoint");
 const getQuerySchema = S.object()
   .id("/sensors")
   .title("getting sensors from the api")
   .additionalProperties(false)
-  .prop("category", S.number())
+  .prop("category_id", S.number())
   .prop("limit", S.number().maximum(supabaseMaxRows))
   .prop("offset", S.number());
 
@@ -44,48 +51,93 @@ const sensors: FastifyPluginAsync = async (fastify) => {
     method: ["GET", "HEAD"],
     logLevel,
     handler: async (request, reply) => {
-      //TODO: Apply category filter
       const {
-        category,
+        category_id,
         limit: qLimit = supabaseMaxRows,
         offset = 0,
       } = request.query;
       const limit = Math.min(qLimit, supabaseMaxRows);
 
-      // get the head only
+      const unit: RangeUnit = "sensor";
+      const selection =
+        "id, external_id, name, description, connection_type, location, longitude, latitude, altitude, category_id, icon_id";
+      // get the head only and return
+      const { count } = await fastify.supabase
+        .from<SensorNoUserId>("sensors")
+        .select(selection, { count: "exact", head: true });
+
       if (request.method === "HEAD") {
-        return reply.status(200).send();
+        const headReplyHeaders = buildReplyHeaders({
+          count,
+          offset,
+          unit,
+        });
+        return reply.status(200).headers(headReplyHeaders).send();
       }
 
-      const {
-        data: sensors,
-        error: sensorError,
-        count,
-      } = await fastify.supabase
-        .from<Sensor>("sensors")
-        .select("*", { count: "exact" })
-        .range(offset, offset + limit);
+      //Apply category filter or not
+      // arguments for the range filter are the same
+      const rangeArgs: [from: number, to: number] = [
+        offset,
+        offset + (limit - 1),
+      ];
+      // isolate the select function
 
+      let pgResponse: PostgrestResponse<SensorNoUserId>;
+      // if we have a category_id filter for it
+      if (category_id) {
+        pgResponse = await fastify.supabase
+          .from<SensorNoUserId>("sensors")
+          .select(selection)
+          .eq("category_id", category_id)
+          .range(...rangeArgs);
+      } else {
+        pgResponse = await fastify.supabase
+          .from<SensorNoUserId>("sensors")
+          .select(selection)
+          .range(...rangeArgs);
+      }
+
+      // now resolve the promise
+      const { data: sensors, error: sensorError } = pgResponse;
+      // fastify.supabase
+      // .from<Omit<Sensor, "user_id">>("sensors")
+      // .select(selection)
+      // .eq("category_id", category_id)
+      // .range(offset, offset + (limit - 1));
+      // ups. There is an error with the requerst to supabase return 500
       if (sensorError) {
         fastify.log.error(sensorError);
         throw fastify.httpErrors.internalServerError();
       }
+      // oh. There is an error in the sensors. Return 500
       if (sensors === null) {
         fastify.log.error("sensors is null but it should not");
         throw fastify.httpErrors.internalServerError();
       }
+      // if we have a count that is higher then the maxRows we need to return a contentRange header and nextPage
+      let contentRange: ContentRange | undefined;
       if (count !== null && count > supabaseMaxRows) {
-        // need to attach some info that there are more values to get
+        contentRange = {
+          offset,
+          limit,
+        };
       }
-
-      const replyPayload = buildReplyPayload(request.url, sensors);
-      reply
-        .status(200)
-        .headers({
-          "Range-Unit": "sensors",
-          "Content-Range": `${0}-${sensors?.length}/${count}`,
-        })
-        .send(replyPayload);
+      // setup the reply headers
+      const replyHeaders = buildReplyHeaders({
+        count,
+        offset,
+        unit: "sensor",
+        unitLength: sensors.length,
+      });
+      // setup the reply payload
+      const replyPayload = buildReplyPayload({
+        url: request.url,
+        payload: sensors,
+        contentRange,
+      });
+      // return the reply
+      reply.status(200).headers(replyHeaders).send(replyPayload);
     },
   });
 };
