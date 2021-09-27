3cac4fe1b0fadb3ea66018d01ee281fa2092cea9
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-09-27 20:14:36 +0200

    STADTPULS-327: tests: records still failing

---
+++ src/lib/__tests__/records.test.ts
@@ -0,0 +1,62 @@
+/* eslint-disable jest/require-top-level-describe */
+/* eslint-disable jest/no-hooks */
+// Copyright (c) 2021 Technologiestiftung Berlin & Fabian Morón Zirfas
+//
+// This software is released under the MIT License.
+
+// https://opensource.org/licenses/MIT
+import { definitions } from "../../common/supabase";
+import {
+  buildServerOpts,
+  closePool,
+  createSensor,
+  sensorsEndpoint,
+  signupUser,
+  truncateTables,
+} from "../../__test-utils";
+import { createRecords } from "../../__test-utils/create-records";
+import buildServer from "../server";
+
+beforeEach(async () => {
+  await truncateTables();
+});
+// eslint-disable-next-line jest/no-hooks
+afterAll(async () => {
+  await truncateTables();
+  await closePool();
+});
+describe(`${sensorsEndpoint}/:sensorId/records tests`, () => {
+  test("should get rejected with 404 due to non existing sensor", async () => {
+    const server = buildServer(buildServerOpts);
+    const response = await server.inject({
+      method: "GET",
+      url: `${sensorsEndpoint}/1/records`,
+    });
+
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toMatchInlineSnapshot(
+      `"{\\"statusCode\\":404,\\"error\\":\\"Not Found\\",\\"message\\":\\"Not Found\\"}"`
+    );
+  });
+  test("should get empty response due to no records", async () => {
+    const server = buildServer(buildServerOpts);
+    const user = await signupUser();
+    const sensor = await createSensor({ user_id: user.id });
+    const records = await createRecords({
+      sensor_id: sensor.id,
+      numberOfRecords: 2000,
+    });
+    const response = await server.inject({
+      method: "GET",
+      url: `${sensorsEndpoint}/${sensor.id}/records`,
+    });
+
+    const json = response.json<{
+      url: string;
+      nextPage: string;
+      data: definitions["records"][];
+    }>();
+    expect(response.statusCode).toBe(200);
+    expect(json.data).toHaveLength(0);
+  });
+});
