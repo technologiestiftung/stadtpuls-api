061ee0b9f11e99cfa65adeb5aa74956da837ee79
ff6347 <fabianmoronzirfas@protonmail.ch>
2021-09-28 14:38:44 +0200

    STADTPULS-327: chore: config

---
+++ wallaby.js
@@ -0,0 +1,14 @@
+module.exports = function (w) {
+  return {
+    autoDetect: true,
+    // files: ["src/**/*.ts"],
+    filesWithNoCoverageCalculated: ["/src/__test-utils/*.ts"],
+    // tests: ["src/lib/__tests__/*.test.ts"],
+    runMode: "onsave",
+    slowTestThreshold: 1000, // 200 ms
+    env: {
+      type: "node",
+    },
+    testFramework: "jest",
+  };
+};
